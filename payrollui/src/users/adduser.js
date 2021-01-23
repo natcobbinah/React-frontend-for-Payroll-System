a51c24d4fa24a6220d8fcc9db0ef9613c916f2a4
natcobbinah <nathaniel.cobbinah@amalitech.org>
2021-01-23 16:42:55 +0000

    add users form designed better

--- payrollui/src/users/adduser.js
+++ payrollui/src/users/adduser.js
@@ -95,13 +95,15 @@ class adduser extends Component{
                   null     
                 }
                 <form>
-                    <div className="form-group">
+                    <div className="form-row">
+                      <div className="form-group col-md-6">
                         <label htmlFor="fullnamelbl">Name</label>
                         <input type="text" class="form-control" id="fullnamelbl" placeholder="FullName here...." onChange={(e) => this.setState({name : e.target.value})}/>
-                    </div>
-                    <div className="form-group">
+                      </div>
+                      <div className="form-group col-md-6">
                         <label htmlFor="addresslbl">Address</label>
                         <input type="text" class="form-control" id="addresslbl" placeholder="Address here...." onChange={(e) => this.setState({address : e.target.value})}/>
+                      </div>
                     </div>
                     <div className="form-row">
                         <div className="form-group col-md-6">
@@ -115,84 +117,80 @@ class adduser extends Component{
                     </div>
 
                     <div className="form-row">
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-3">
                           <label htmlFor="genderlbl">Gender</label>
                           <input type="text" className="form-control" id="genderlbl" placeholder="Gender" onChange={(e) => this.setState({gender : e.target.value})}/>
                         </div>
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-3">
                           <label htmlFor="dateofbirthlbl">Date of Birth</label>
                           <input type="date" className="form-control" id="dateofbirthlbl" onChange={(e) => this.setState({birthdate : e.target.value})}/>
                         </div>
-                    </div>
-
-                    <div className="form-row">
-                        <div className="form-group col-md-4">
-                            <label htmlFor="citylbl">City</label>
-                            <input type="text" className="form-control" id="citylbl" placeholder="City name here..." onChange={(e) => this.setState({city : e.target.value})}/>
-                        </div>
-                        <div className="form-group col-md-4">
-                            <label htmlFor="phonenumberlbl">PhoneNumber</label>
-                            <input type="text" className="form-control" id="phonenumberlbl" placeholder="Phoneno here..." onChange={(e) => this.setState({phonenumber : e.target.value})}/>
-                        </div>
-                        <div className="form-group col-md-4">
-                            <label htmlFor="maritalstatuslbl">Marital Status</label>
-                            <input type="text" className="form-control" id="maritalstatuslbl" placeholder="Status here.." onChange={(e) => this.setState({maritalstatus : e.target.value})}/>
-                        </div>
-                    </div>
-                    <div className="form-row">
-                        <div className="form-group col-md-4">
-                            <label htmlFor="employeeidlbl">EmployeeID</label>
-                            <input type="text" className="form-control" id="employeeidlbl" placeholder="EmployeeID here.." onChange={(e) => this.setState({employeeid : e.target.value})}/>
-                        </div>
-                        <div className="form-group col-md-4">
+                        <div className="form-group col-md-3">
                             <label htmlFor="employeelvlbl">Employee Level</label>
                             <input type="text" className="form-control" id="employeelvlbl" placeholder="Position" onChange={(e) => this.setState({employeelevel : e.target.value})}/>
                         </div>
-                        <div className="form-group col-md-4">
+                        <div className="form-group col-md-3">
                             <label htmlFor="hiredatelbl">HireDate</label>
                             <input type="date" className="form-control" id="hiredatelbl" onChange={(e) => this.setState({hiredate : e.target.value})}/>
                         </div>
                     </div>
+
                     <div className="form-row">
-                        <div className="form-group col-md-4">
+                        <div className="form-group col-md-3">
+                            <label htmlFor="citylbl">City</label>
+                            <input type="text" className="form-control" id="citylbl" placeholder="City name here..." onChange={(e) => this.setState({city : e.target.value})}/>
+                        </div>
+                        <div className="form-group col-md-3">
+                            <label htmlFor="phonenumberlbl">PhoneNumber</label>
+                            <input type="text" className="form-control" id="phonenumberlbl" placeholder="Phoneno here..." onChange={(e) => this.setState({phonenumber : e.target.value})}/>
+                        </div>
+                        <div className="form-group col-md-3">
+                            <label htmlFor="maritalstatuslbl">Marital Status</label>
+                            <input type="text" className="form-control" id="maritalstatuslbl" placeholder="Status here.." onChange={(e) => this.setState({maritalstatus : e.target.value})}/>
+                        </div>
+                        <div className="form-group col-md-3">
+                            <label htmlFor="employeeidlbl">EmployeeID</label>
+                            <input type="text" className="form-control" id="employeeidlbl" placeholder="EmployeeID here.." onChange={(e) => this.setState({employeeid : e.target.value})}/>
+                        </div>
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-md-3">
                             <label htmlFor="bankaccnolbl">BankAccountNo</label>
                             <input type="text" className="form-control" id="bankaccnolbl" placeholder="356456453563" onChange={(e) => this.setState({bankaccountnumber : e.target.value})}/>
                         </div>
-                        <div className="form-group col-md-4">
+                        <div className="form-group col-md-3">
                             <label htmlFor="ssnitlbl">SSNIT_ID</label>
                             <input type="text" className="form-control" id="ssnitlbl" placeholder="25452354525" onChange={(e) => this.setState({ssnitid : e.target.value})}/>
                         </div>
-                        <div className="form-group col-md-4">
+                        <div className="form-group col-md-3">
                             <label htmlFor="votersidlbl">VotersID</label>
                             <input type="text" className="form-control" id="votersidlbl" onChange={(e) => this.setState({votersid : e.target.value})}/>
                         </div>
-                    </div>
-                    <div className="form-row">
-                        <div className="form-group col-md-4">
+                        <div className="form-group col-md-3">
                             <label htmlFor="birthcertidlbl">BirthCertificate</label>
                             <input type="text" className="form-control" id="birthcertidlbl" placeholder="356456453563" onChange={(e) => this.setState({birthcertid : e.target.value})}/>
                         </div>
-                        <div className="form-group col-md-4">
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-md-3">
                             <label htmlFor="driverslicenlbl">DriversLicense</label>
                             <input type="text" className="form-control" id="driverslicenlbl" placeholder="25452354525" onChange={(e) => this.setState({driverslicenseid : e.target.value})}/>
                         </div>
-                        <div className="form-group col-md-4">
+                        <div className="form-group col-md-3">
                             <label htmlFor="passportlbl">PassportNo</label>
                             <input type="text" className="form-control" id="passportlbl" onChange={(e) => this.setState({passportid : e.target.value})}/>
                         </div>
-                    </div>
-                    <div className="form-row">
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-3">
                             <label htmlFor="tinnolbl">Tin Number</label>
                             <input type="text" className="form-control" id="tinnolbl" placeholder="25452354525" onChange={(e) => this.setState({tinnumber : e.target.value})}/>
                         </div>
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-3">
                             <label htmlFor="marriagecertID">Marriage CertificateID</label>
                             <input type="text" className="form-control" id="marriagecertID" onChange={(e) => this.setState({marriagecertid : e.target.value})}/>
                         </div>
                     </div>
                     <div className="form-row">
-                       <div className="form-group col-md-12">
+                       <div className="form-group col-md-3">
                             <button type="button" className="btn btn-primary" onClick={this.getFormDataandSavetoDB}>Add User</button>
                         </div>
                     </div>
